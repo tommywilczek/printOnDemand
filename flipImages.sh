f1a54a55edbe49ea5376e3dfc3ad22f1966b4c67
Tommy Wilczk <tommywilczek@utexas.edu>
2019-04-30 15:12:08 -0500

    Created library of ColorBrewer colors

--- flipImages.sh
+++ flipImages.sh
@@ -2,7 +2,7 @@
 for filename in /Users/tommywilczek/Documents/Projects/patternPop/generatedPatterns/*.png; do 
     echo $filename
     # mirrorName="$filename-mirror"
-    wordToInsert="-mirror"
+    wordToInsert="_mirror"
     match=".png"
     mirrorName=${filename%%"${match}"*}${wordToInsert}${filename#*"${filename%%"${match}"*}"}
     # ^ Inserts the mirror tag in front of the .png in the filename
