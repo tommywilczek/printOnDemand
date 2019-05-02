26732759e00556317a4825f1b7935afd36b5b127
Tommy Wilczk <tommywilczek@utexas.edu>
2019-05-02 18:24:52 -0500

    Created browser automation to log in to printful

--- printfulAutomation.py
+++ printfulAutomation.py
@@ -1,5 +1,19 @@
 from selenium import webdriver
 
+from selenium.webdriver.common.keys import Keys
+
+printfulPassword = input("enter printful password in quotes:")
+print(printfulPassword)
+
 browser = webdriver.Chrome('/Users/tommywilczek/Documents/Projects/chromedriver')
 
-browser.get("https://www.printful.com/dashboard/store")
+browser.get("https://www.printful.com/auth/login")
+
+emailField = browser.find_element_by_id("customer-email")
+
+passwordField = browser.find_element_by_id("customer-password")
+
+emailField.send_keys("tommywilczek@gmail.com")
+
+passwordField.send_keys(printfulPassword, Keys.ENTER)
+
