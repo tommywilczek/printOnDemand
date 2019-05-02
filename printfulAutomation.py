a1080228a97cd218412faf3787a77e57715fa73e
Tommy Wilczk <tommywilczek@utexas.edu>
2019-05-02 18:28:06 -0500

    Refactored login into its own function

--- printfulAutomation.py
+++ printfulAutomation.py
@@ -4,16 +4,22 @@ from selenium.webdriver.common.keys import Keys
 
 printfulPassword = input("enter printful password in quotes:")
 print(printfulPassword)
-
+    
 browser = webdriver.Chrome('/Users/tommywilczek/Documents/Projects/chromedriver')
 
-browser.get("https://www.printful.com/auth/login")
+def main():
 
-emailField = browser.find_element_by_id("customer-email")
+    loginToPrintful(printfulPassword)
 
-passwordField = browser.find_element_by_id("customer-password")
+def loginToPrintful(printfulPassword):
+    browser.get("https://www.printful.com/auth/login")
 
-emailField.send_keys("tommywilczek@gmail.com")
+    emailField = browser.find_element_by_id("customer-email")
 
-passwordField.send_keys(printfulPassword, Keys.ENTER)
+    passwordField = browser.find_element_by_id("customer-password")
 
+    emailField.send_keys("tommywilczek@gmail.com")
+
+    passwordField.send_keys(printfulPassword, Keys.ENTER)
+
+main()
