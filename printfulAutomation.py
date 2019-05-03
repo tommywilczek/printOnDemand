a6c73560de25f1d8d7d9e40d02fec7b5d6be4df9
Tommy Wilczk <tommywilczek@utexas.edu>
2019-05-03 17:56:35 -0500

    Began ordering fitness mens shirt

--- printfulAutomation.py
+++ printfulAutomation.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import configparser
 
 parser = configparser.ConfigParser()
@@ -14,10 +15,18 @@ browser = webdriver.Chrome('/Users/tommywilczek/Documents/Projects/chromedriver'
 def main():
 
     loginToPrintful(printfulPassword)
+
     waitForPageLoad()
+
     goToChooseProduct()
+
     waitForPageLoad()
+
     navigateToMensAllOverShirts()
+
+    waitForPageLoad()
+
+    createAllOverPrintMensAthleticTShirt()
 
 def loginToPrintful(printfulPassword):
     browser.get("https://www.printful.com/auth/login")
@@ -50,6 +59,31 @@ def navigateToMensAllOverShirts():
     allOverShirtsButton = browser.find_element_by_xpath('//*[@id="modal-1"]/div/div/div[1]/div[2]/div/div[3]/div/div[1]/div[3]/div/div[2]/div[1]/div/a[5]')
 
     allOverShirtsButton.click()
+
+def createAllOverPrintMensAthleticTShirt():
+    # Prerequisite: navigateToMensAllOverShirts
+    allOverPrintMensAthleticTShirtButton = browser.find_element_by_xpath("//*[contains(text(), 'Athletic T-Shirt')]")
+
+    allOverPrintMensAthleticTShirtButton.click()
+
+    waitForPageLoad()
+
+    modal = browser.find_element_by_xpath('//*[@class="modal-body"]')
+
+
+    # modal.send_keys(Keys.END)
+
+    # designTab = browser.find_element_by_xpath("//*[contains(text(), 'Upload file')]")
+
+    # designTab.click()
+
+    whiteRadioButton = browser.find_element_by_xpath(".//input[@type='radio' and @value='white']")
+
+    whiteRadioButton.send_keys(Keys.SPACE) # click radio button
+
+    uploadFileButton = browser.find_element_by_xpath("//*[contains(text(), 'Upload file')]")
+
+    uploadFileButton.click()
 
 def waitForPageLoad():
     time.sleep(1)
