a5769baa2d93a36163b051d8ed59f74bad6c5fe7
Tommy Wilczk <tommywilczek@utexas.edu>
2019-05-02 21:25:27 -0500

    hid api key in config file

--- printfulAPI.py
+++ printfulAPI.py
@@ -1,38 +1,39 @@
 import requests
 import json
 import base64
+import configparser
 
-# apiKey = 
-encodedApiKey = base64.b64encode(bytes(apiKey))
+parser = configparser.ConfigParser()
+
+parser.read('config.ini')
+
+apiKey = parser.get('api', 'api_key')
+
+encodedApiKey = base64.b64encode(bytes(apiKey, 'utf-8'))
+
 urlBase = 'https://api.printful.com/'
 
 headers = {'Content-Type': 'application/json',
-           'Authorization': 'Basic {0}'.format(encodedApiKey)}
+        #    'Authorization': 'Basic {0}'.format(encodedApiKey) HHEADER"S BROKEN
+           }
+
+def main():
+    allProducts = getAllProducts()
+    print(allProducts)
 
 def getAllProducts():
     api_url = _url('products')
     response = requests.get(api_url, headers=headers)
 
     if response.status_code == 200:
+        print('YEAH')
         return json.loads(response.content.decode('utf-8'))
     else:
+        print('HOONE')
         return None
 
 def _url(path = ''):
     return urlBase + path
 
 
-allProducts = getAllProducts()
-
-print(allProducts)
-# jsonData = urllib.urlopen(url)
-
-# loadedJsonData = json.load(jsonData)
-
-# def getAllProducts():
-#     urllib.urlopen(_url('products'))
-
-# def main():
-#     allProducts = getAllProducts()
-#     # print(json.load(allProducts))
-# main()
+main()
