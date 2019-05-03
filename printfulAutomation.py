1244af6c63e3ff5c1c7556f465bfce3f380b8c10
Tommy Wilczk <tommywilczek@utexas.edu>
2019-05-03 13:37:39 -0500

    added go to add product

--- printfulAutomation.py
+++ printfulAutomation.py
@@ -1,19 +1,21 @@
 from selenium import webdriver
-
+import time
 from selenium.webdriver.common.keys import Keys
+import configparser
 
-printfulPassword = input("enter printful password in quotes:")
-print(printfulPassword)
-    
+parser = configparser.ConfigParser()
+
+parser.read('config.ini')
+
+printfulPassword = parser.get('passwords', 'printfulPassword')
+
 browser = webdriver.Chrome('/Users/tommywilczek/Documents/Projects/chromedriver')
 
 def main():
 
     loginToPrintful(printfulPassword)
-
-    browser.get("https://www.printful.com/dashboard/store")
-
-    browser.find_elements_by_xpath("//*[contains(text(), 'Add')]")
+    waitForPageLoad()
+    goToChooseProduct()
 
 def loginToPrintful(printfulPassword):
     browser.get("https://www.printful.com/auth/login")
@@ -25,5 +27,20 @@ def loginToPrintful(printfulPassword):
     emailField.send_keys("tommywilczek@gmail.com")
 
     passwordField.send_keys(printfulPassword, Keys.ENTER)
+
+def goToChooseProduct():
+    patternPopStoreId = '1354143'
+    
+    browser.get("https://www.printful.com/dashboard/sync?store=" + patternPopStoreId)
+    
+    waitForPageLoad()
+
+    addProductButton = browser.find_element_by_xpath("//*[text()='Add product']")
+
+    addProductButton.click()
+
+
+def waitForPageLoad():
+    time.sleep(1)
 
 main()
