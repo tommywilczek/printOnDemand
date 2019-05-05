48f884beef4fd5b5e4ef359423c40680fbac920b
Tommy Wilczk <tommywilczek@utexas.edu>
2019-05-05 11:04:52 -0500

    Can create an athletic shirt

--- printfulAutomation.py
+++ printfulAutomation.py
@@ -107,6 +107,18 @@ def createAllOverPrintMensAthleticTShirt(colorName):
 
     createProductDescription(productType, productStyle, colorName, gender)
 
+    proceedToPricingButton = browser.find_element_by_xpath("//*[contains(text(), 'Proceed to pricing')]")
+
+    proceedToPricingButton.click()
+
+    waitForPageLoad()
+
+    upchargeByPercentage()
+
+    submitItemButton = browser.find_element_by_xpath("//*[contains(text(), 'Submit to store')]")
+
+    submitItemButton.click()
+
 def chooseBackOfItemColor(colorName):
     backOfItemTab = browser.find_element_by_xpath('//*[@id="modal-1"]/div/div/div[1]/div[2]/div/div[3]/div/div/div[2]/div[2]/div[1]/div/div[1]/ul/div/li[2]/a/span')
 
@@ -186,9 +198,19 @@ def generateProductDescription(productType, productStyle, colorName, gender=None
 
     return colorName, ' ' ,productStyle, ' ' ,productKeywords, ' ' ,genderDescription 
 
+def upchargeByPercentage():
+    # Must be in 'pricing' stage of create product
+    dollarOrPercentageDropdown = browser.find_element_by_xpath('//*[@id="modal-1"]/div/div/div[1]/div[2]/div/div[3]/div/div[2]/div[2]/div[2]/table/thead/tr[2]/td[4]/span[2]/select/option[1]') 
+
+    dollarOrPercentageDropdown.click()
+
+    numberToIncreaseField = browser.find_element_by_class_name('profit-amount')
+
+    numberToIncreaseField.clear()
+
+    numberToIncreaseField.send_keys('15')
+
 def waitForPageLoad():
     time.sleep(1)
 
 main()
-
-generateProductDescription('shirt', 'Yellow-Orange-Red', 'Mens')
